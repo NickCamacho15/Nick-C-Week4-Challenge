4d6dd77cc67993f43a16c2fd3583dfaf091fc8d6
Nick Camacho <118080701+NickCamacho15@users.noreply.github.com>
2023-05-30 09:40:25 -0400

    Added files for coding quiz
    Added necessary files in order to build and deploy application.

---
+++ questions.js
@@ -0,0 +1,28 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: 'Which HTML element is used to define important text?',
+    choices: ['<strong>', '<b>', '<i>', '<important>'],
+    answer: '<strong>',
+  },
+  {
+    title: 'Which CSS property controls the text size?',
+    choices: ['text-style', 'font-style', 'text-size', 'font-size'],
+    answer: 'font-size',
+  },
+  {
+    title: 'How do you write "Hello World" in an alert box in JavaScript?',
+    choices: ['alert("Hello World")', 'msgBox("Hello World")', 'alertBox("Hello World")', 'msg("Hello World")'],
+    answer: 'alert("Hello World")',
+  },
+  {
+    title: 'What does CSS stand for?',
+    choices: ['Cascading Style Sheets', 'Colorful Style Sheets', 'Creative Style Sheets', 'Computer Style Sheets'],
+    answer: 'Cascading Style Sheets',
+  },
+  {
+    title: 'Which is not a JavaScript data type?',
+    choices: ['Number', 'String', 'Boolean', 'Character'],
+    answer: 'Character',
+  }
+];
